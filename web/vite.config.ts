b536796e3187d037f8414d6c6e7986a66cfdd6fa
mason.zhu <ws_zhu@foxmail.com>
2026-07-23 17:02:14 +0800

    chore(deploy): 修复vite配置问题

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     AutoImport({
       imports: [
         {
-          antd: ['message', 'notification', 'Modal', 'Form'],
+          antd: ['message', 'notification', 'Modal', 'Form', 'ConfigProvider'],
         },
       ],
       dts: 'src/auto-imports.d.ts',
